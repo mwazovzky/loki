3bd825f62ff22a9c8e54dc34adde0fa45e38dde8
Alexander Nichiporenko <alexander.nichiporenko@skillbox.ru>
2021-05-11 15:52:48 +0300

    Extract package users

--- main.go
+++ main.go
@@ -5,38 +5,33 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/mwazovzky/loki/users"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-type User struct {
-	ID       int
-	Name     string
-	Email    string
-	Password string
-}
-
-func mainHandler(rw http.ResponseWriter, r *http.Request) {
+func usersHandler(rw http.ResponseWriter, r *http.Request) {
+	db := connectDB()
+	u := users.New(db)
+	data := u.Find()
+	fmt.Println(data)
 	fmt.Fprintln(rw, "Hello World!")
-
-	dsn := "loki:password@tcp(mysql:3306)/loki"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		fmt.Println("ERROR: db connection error", err)
-	}
-
-	var users []User
-	result := db.Find(&users)
-
-	fmt.Println(users)
-	fmt.Println("RowsAffected:", result.RowsAffected, "ERROR", result.Error)
 }
 
 func main() {
 	port := ":3000"
 
-	http.HandleFunc("/", mainHandler)
+	http.HandleFunc("/users", usersHandler)
 
 	log.Println("Listening on port", port)
 	http.ListenAndServe(port, nil)
+}
+
+func connectDB() *gorm.DB {
+	dsn := "loki:password@tcp(mysql:3306)/loki"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("ERROR: db connection error")
+	}
+	return db
 }
