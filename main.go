5262fc9d73321bfd33d2fc9585ae6d149e946b91
Alexander Nichiporenko <alexander.nichiporenko@skillbox.ru>
2021-05-11 15:37:54 +0300

    GORM. Load user index

--- main.go
+++ main.go
@@ -4,10 +4,32 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
+
+type User struct {
+	ID       int
+	Name     string
+	Email    string
+	Password string
+}
 
 func mainHandler(rw http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(rw, "Hello World!")
+
+	dsn := "loki:password@tcp(mysql:3306)/loki"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println("ERROR: db connection error", err)
+	}
+
+	var users []User
+	result := db.Find(&users)
+
+	fmt.Println(users)
+	fmt.Println("RowsAffected:", result.RowsAffected, "ERROR", result.Error)
 }
 
 func main() {
