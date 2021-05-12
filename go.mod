607454df481c189b56a903b7d68b1d1dd9868b39
Alexander Nichiporenko <alexander.nichiporenko@skillbox.ru>
2021-05-12 20:28:59 +0300

    Structure routes

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/mwazovzky/loki
 go 1.16
 
 require (
+	github.com/gorilla/mux v1.8.0 // indirect
 	gorm.io/driver/mysql v1.0.6 // indirect
 	gorm.io/gorm v1.21.9 // indirect
 )
--- main.go
+++ main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"strconv"
 	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/mwazovzky/loki/users"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -21,12 +24,66 @@ func homepageHandler(rw http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(rw, "Home Page")
 }
 
-func usersHandler(rw http.ResponseWriter, r *http.Request) {
-	log.Println("Users Index Page")
-	u := users.New(db)
-	data := u.Find()
-	fmt.Println(data)
-	fmt.Fprintln(rw, "Users Page")
+func userIndex(rw http.ResponseWriter, r *http.Request) {
+	log.Println("User Index Page")
+	fmt.Fprintln(rw, "User Index Page")
+
+	// u := users.New(db)
+	// data := u.Find()
+
+	data := []users.User{
+		{1, "Mary", "mary@example.com", "password"},
+		{2, "Vasy", "vasy@example.com", "password"},
+		{3, "Alex", "alex@example.com", "password"},
+	}
+
+	rw.Header().Add("Content-Type", "application/json")
+
+	e := json.NewEncoder(rw)
+	err := e.Encode(data)
+	if err != nil {
+		http.Error(rw, "Unable to marshall json", http.StatusInternalServerError)
+	}
+}
+
+func userShow(rw http.ResponseWriter, r *http.Request) {
+	log.Println("Users Show Page")
+	fmt.Fprintln(rw, "User Show Page")
+
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		panic(err)
+	}
+
+	// u := users.New(db)
+	// data := u.Find()
+
+	data := users.User{id, "Test", "test@example.com", "password"}
+
+	rw.Header().Add("Content-Type", "application/json")
+
+	e := json.NewEncoder(rw)
+	err = e.Encode(data)
+	if err != nil {
+		http.Error(rw, "Unable to marshall json", http.StatusInternalServerError)
+	}
+}
+
+func userCreate(rw http.ResponseWriter, r *http.Request) {
+	u := users.User{}
+	err := json.NewDecoder(r.Body).Decode(&u)
+	if err != nil {
+		panic(err)
+	}
+
+	rw.Header().Add("Content-Type", "application/json")
+
+	e := json.NewEncoder(rw)
+	err = e.Encode(u)
+	if err != nil {
+		http.Error(rw, "Unable to marshall json", http.StatusInternalServerError)
+	}
 }
 
 func main() {
@@ -34,12 +91,16 @@ func main() {
 	port := ":3000"
 
 	// Setup db connection
-	db = connectDB()
+	// db = connectDB()
 
 	// Setup routing
-	sm := http.NewServeMux()
+	sm := mux.NewRouter()
 	sm.HandleFunc("/", homepageHandler)
-	sm.HandleFunc("/users", usersHandler)
+
+	apiRouter := sm.PathPrefix("/api").Subrouter()
+	apiRouter.HandleFunc("/users", userIndex).Methods(http.MethodGet)
+	apiRouter.HandleFunc("/users", userCreate).Methods(http.MethodPost)
+	apiRouter.HandleFunc("/users/{id:[0-9]+}", userShow).Methods(http.MethodGet)
 
 	// Configure http server
 	server := &http.Server{
